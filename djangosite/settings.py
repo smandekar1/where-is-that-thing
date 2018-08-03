0e9b849f53f383aa296ae3ec71799439c5fb058b
smandekar1 <smandekar1@yahoo.com>
2018-08-03 14:35:25 -0500

    make it

--- djangosite/settings.py
+++ djangosite/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 import django_heroku
 import os
+import psycopg2
+
+
+
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +135,7 @@ MEDIA_URL = '/media/'
 # Configure Django App for Heroku.
 
 django_heroku.settings(locals())
+
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
